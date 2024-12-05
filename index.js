adc30991c7d639d2fdd34039e2148552e870b9cc
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-12-05 14:42:50 +0600

    implement the jsonwebtoken

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
-require('dotenv').config(); 
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin:["http://localhost:5173"],
-    credentials:true
+  origin: ["http://localhost:5173"],
+  credentials: true
 }))
 app.use(express.json())
 
@@ -26,40 +27,51 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-     const tutorCollection = client.db("TutorHub").collection('tutorInfo')
-     const studentCollection = client.db("TutorHub").collection('studentInfo')
+    const tutorCollection = client.db("TutorHub").collection('tutorInfo')
+    const studentCollection = client.db("TutorHub").collection('studentInfo')
 
-     app.post('/tutorInfo', async (req,res)=>{
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JSON_WEB_TOKEN, { expiresIn: "24h" })
+      // console.log(token);
+      res.send({ token })
+
+    })
+
+    
+
+    app.post('/tutorInfo', async (req, res) => {
       const tutor = req.body;
-      const query = {email: tutor.email}
+      const query = { email: tutor.email }
       const existingTutor = await tutorCollection.findOne(query)
-      if(existingTutor){
-        return res.send({message:"This email is existing" , insertedId: null} )
+      if (existingTutor) {
+        return res.send({ message: "This email is existing", insertedId: null })
       }
-      else{
+      else {
         const insertTutor = await tutorCollection.insertOne(tutor)
         res.send(insertTutor)
       }
-     })
+    })
 
-     app.post("/studentInfo", async(req,res)=>{
+    app.post("/studentInfo", async (req, res) => {
       const student = req.body
-      const query = {email: student.email}
+      const query = { email: student.email }
       const existingStudent = await studentCollection.findOne(query)
-      if(existingStudent){
-        return res.send({message: "User already exist", insertedId: null})
+      if (existingStudent) {
+        return res.send({ message: "User already exist", insertedId: null })
       }
-      else{
+      else {
         const insertStudent = await studentCollection.insertOne(student)
         res.send(insertStudent)
       }
-     })
+    })
 
-     app.get('/getTutor', async (req,res)=>{
+    app.get('/getTutor', async (req, res) => {
       const getTutor = req.body;
       const result = await tutorCollection.find().toArray()
       res.send(result)
-     })
+    })
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -74,10 +86,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
-    res.send("Tutor Hub is on")
+app.get('/', (req, res) => {
+  res.send("Tutor Hub is on")
 })
 
-app.listen(port,()=>{
-    console.log(`Hello World ${port}`)
+app.listen(port, () => {
+  console.log(`Hello World ${port}`)
 })
